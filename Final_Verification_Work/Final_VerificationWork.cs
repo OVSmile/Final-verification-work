498e9a61c1ba14518a56e9f13c82b9de2bcf8379
OVSmile <114104859+OVSmile@users.noreply.github.com>
2022-11-23 23:17:55 +0300

    Add program code

---
+++ Final_Verification_Work/Final_VerificationWork.cs
@@ -0,0 +1,39 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше или равна 3 символам.
+Console.Clear();
+
+string[] Array = {"hello", "2", "world", ":-)"};
+PrintArray(Array);
+Console.Write("->");
+Array = GetModifiedArray(Array);
+PrintArray(Array);
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        if(i == array.Length - 1)
+            Console.Write($"\"{array[i]}\"");
+        else 
+            Console.Write($"\"{array[i]}\",");
+        
+    }
+    Console.Write("]");
+}
+
+string[] GetModifiedArray(string[] arr)
+{
+    List<string> temp = new List<string>();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3) 
+        {
+            temp.Add(arr[i]);   
+        }
+    }
+    arr = temp.ToArray();
+    return arr;
+}
+
